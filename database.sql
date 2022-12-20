85b5e098699c7ece59332d001c970df9a7594d04
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2022-12-20 23:05:33 +0100

    unique constraint for sql

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ CREATE TABLE users(
 	user_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
-	username TEXT NOT NULL,
+	username TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL
 );
 
